36ce9b1cfc706e2ae7056b3a3ea6923d7be56d55
Tuyen Le <lethanhtuyen10081998@gmail.com>
2024-10-03 21:26:09 +0700

    run migration

--- src/migrations/1727965408524-CreateTableUser.ts
+++ src/migrations/1727965408524-CreateTableUser.ts
@@ -1,14 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTableUser1727965408524 implements MigrationInterface {
-    name = 'CreateTableUser1727965408524'
+  name = 'CreateTableUser1727965408524';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`auth\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE \`user\` (
+        \`id\` int NOT NULL AUTO_INCREMENT,
+        \`username\` varchar(255) NOT NULL,
+        \`email\` varchar(255) NOT NULL,
+        \`password\` varchar(255) NOT NULL,
+        \`address\` varchar(255),
+        \`full_name\` varchar(255) NOT NULL,
+        \`created_at\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        \`updated_at\` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        PRIMARY KEY (\`id\`)
+      ) ENGINE=InnoDB
+    `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE \`auth\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`user\``);
+  }
 }
